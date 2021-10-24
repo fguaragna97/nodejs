0f60fcd794f6f310a2322fcb5ca10d314f9f3eb2
Franco Guaragna <55494072+fguaragna97@users.noreply.github.com>
2021-10-23 23:03:36 -0700

    yues

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-const colors = require("colors/safe");
-
-const args = process.argv.slice(2);
-const [name = "Friend"] = args;
-const hour = new Date().getHours();
-
-// Ask for hours range
-if (hour >= 6 && hour < 12) {
-  console.log(colors.yellow(`Good morning ${name}`));
-} else if (hour >= 12 && hour < 18) {
-  console.log(colors.green(`Good afternoon ${name}`));
-} else if (hour >= 18 && hour < 23) {
-  console.log(colors.cyan(`Good evening ${name}`));
-} else {
-  console.log(colors.blue(`Good night ${name}`));
+function sumAsync(a, b, callback) {
+  setTimeout(function () {
+    callback(a + b);
+  }, 1000);
 }
+
+console.log("before");
+sumAsync(40, 2, function (result) {
+  console.log("Result:", result);
+});
+
+console.log("after");
